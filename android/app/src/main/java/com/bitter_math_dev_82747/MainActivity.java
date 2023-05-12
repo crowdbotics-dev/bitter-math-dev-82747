d1f822d81261d708679c7606f26f3af18be4b62f
Crowdbotics <engineering@crowdbotics.com>
2023-05-12 14:13:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bitter_math_dev_82747/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bitter_math_dev_82747;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bitter_math_dev_82747";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-math-dev-82747",
+  url: "https://bitter-math-dev-82747.botics.co",
+  api: "https://bitter-math-dev-82747.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
